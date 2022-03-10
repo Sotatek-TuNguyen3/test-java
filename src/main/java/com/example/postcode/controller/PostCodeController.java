9c15f8775d6e38db1f9030e0fbfae9f2c37e0823
Tu.Nguyen3 <tu.nguyen3@sotatek.com>
2022-03-10 16:33:44 +0700

    delete wrong line

--- src/main/java/com/example/postcode/controller/PostCodeController.java
+++ src/main/java/com/example/postcode/controller/PostCodeController.java
@@ -7,8 +7,10 @@ import com.example.postcode.service.PostCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+@RestController
 @RequestMapping("/api")
 public class PostCodeController {
 
